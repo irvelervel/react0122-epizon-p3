0bcf68ffd228412f85c4629d65a54538a21627eb
Stefano Casasola <ste.casasola@gmail.com>
2022-09-13 12:56:25 +0200

    Finisce il progetto Epizon

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ import './style/index.css'
 // ora è necessario injectare il nostro store
 // all'interno della react-app
 import { Provider } from 'react-redux' // libreria di binding
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <App />
+    {/* ora inseriamo un ulteriore layer dentro Redux, per garantire la persistenza */}
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,9 +5,23 @@ import bookReducer from '../reducers/bookReducer'
 import cartReducer from '../reducers/cartReducer' // basta puntare la cartella
 import userReducer from '../reducers/userReducer' // basta puntare la cartella
 // dove è contenuto il file index.js
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 
 // ora che abbiamo userReducer e cartReducer che mantengono ciascuno una porzione del
 // Redux Store, manca solamente come ricombinarli insieme...!
+
+const persistConfig = {
+  key: 'root', // il livello da cui vogliamo cominciare a far persistere i dati
+  storage, // come dire --> storage: storage, seleziona lo storage engine da utilizzare
+  transforms: [
+    // elenco delle trasformazioni (plugin) applicabili a redux-persist
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+}
 
 const bigReducer = combineReducers({
   cart: cartReducer,
@@ -15,8 +29,16 @@ const bigReducer = combineReducers({
   book: bookReducer,
 })
 
-const store = configureStore({
-  reducer: bigReducer, // perchè c'è spazio per uno solo!
+const persistedReducer = persistReducer(persistConfig, bigReducer)
+// una versione "persistente" di bigReducer
+
+export const store = configureStore({
+  reducer: persistedReducer, // perchè c'è spazio per uno solo!
+  // questo risolve l'errore del non-serializable value
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // <-- lo spegne
+    }),
 })
 
-export default store
+export const persistor = persistStore(store)
